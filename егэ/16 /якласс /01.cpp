0245eb496ec1cdff9908221ee9ad734047a44478
Qonixxx <159039354+Qonixxx@users.noreply.github.com>
2024-10-21 16:01:36 +0700

    Create 01.cpp

---
+++ егэ/16 /якласс /01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int f(int n) {
+	if (n == 1) return 2;
+	if (n % 2 == 0) return 2 * n + f(n - 3);
+	if (n > 1 && !(n % 2 == 0)) return 2 * f(n + 1);
+}
+
+int main() {
+	std::cout << f(21); // ans = 26520
+	return 0;
+}
