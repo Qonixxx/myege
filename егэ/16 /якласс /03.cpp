29e1a4635be5b6ce8080d8891d300255ab5fc44c
Qonixxx <159039354+Qonixxx@users.noreply.github.com>
2024-10-21 16:13:48 +0700

    Create 03.cpp

---
+++ егэ/16 /якласс /03.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int f(int n) {
+	if (n == 1) return 1;
+	if (n == 2) return 4;
+	if (n > 2) return f(n - 1) * n + f(n - 2);
+}
+
+int main() {
+	std::cout << f(7);
+  return 0;
+}
