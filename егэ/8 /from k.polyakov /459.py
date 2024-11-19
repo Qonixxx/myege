4281a13ae0b2c485379ee73cd6ae3196adbf7259
Qonixxx <159039354+Qonixxx@users.noreply.github.com>
2024-11-19 20:09:48 +0700

    Update 459.py

--- егэ/8 /from k.polyakov /459.py
+++ егэ/8 /from k.polyakov /459.py
@@ -1,9 +1,13 @@
+'''
+Определите количество 15-ричных пятизначных чисел, в записи которых ровно одна цифра 8 и не менее двух цифр с числовым значением, превышающим 9.
+'''
+
 from itertools import *
 k = 0
 
 for x in product("0123456789abcde", repeat = 5):
     s = "".join(x)
-    if s.count("9") == 1 and s[0] != "0" and s.count("a") + s.count("b") \
+    if s.count("8") == 1 and s[0] != "0" and s.count("a") + s.count("b") \
        + s.count("c") + s.count("d") + s.count("e") >= 2:
         k += 1
 print(k)
