2c13dfa1f2c6b3d4c1d8d5a7fc5c697f4a53ccc0
Qonixxx <159039354+Qonixxx@users.noreply.github.com>
2024-11-28 22:31:01 +0700

    Create 1921.py

---
+++ егэ/tests /from Yandex (3-rd) /1921.py
@@ -0,0 +1,10 @@
+from math import ceil
+def f(s, m):
+  if s <= 35: return m % 2 == 0
+  if m == 0: return 0
+  h = [f(s - 2, m - 1), f(s - 4, m - 1), f(ceil(s / 2), m - 1)]
+  return any(h) if (m - 1) % 2 == 0 else all(h)
+
+print("19:",[s for s in range(100, 35, -1) if not f(s, 1) and f(s, 2)])
+print("20:", [s for s in range(100, 35, -1) if not f(s, 1) and f(s, 3)])
+print("21:",[s for s in range(100, 35, -1) if not f(s, 2) and f(s, 4)])
