f5cebb21c93d418ae11cf03c3ae6e7493fd4376d
Qonixxx <159039354+Qonixxx@users.noreply.github.com>
2024-11-28 23:26:03 +0700

    Update 1921.py

--- егэ/tests /from Yandex (3-rd) /1921.py
+++ егэ/tests /from Yandex (3-rd) /1921.py
@@ -6,5 +6,5 @@ def f(s, m):
   return any(h) if (m - 1) % 2 == 0 else all(h)
 
 print("19:",[s for s in range(100, 35, -1) if not f(s, 1) and f(s, 2)])
-print("20:", [s for s in range(100, 35, -1) if not f(s, 1) and f(s, 3)])
+print("20:", [s for s in range(300, 35, -1) if not f(s, 1) and f(s, 3)])
 print("21:",[s for s in range(100, 35, -1) if not f(s, 2) and f(s, 4)])
