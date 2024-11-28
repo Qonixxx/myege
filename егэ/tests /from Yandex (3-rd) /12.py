2ebc403f7ed2110326a816319a4d7a06b8955ee0
Qonixxx <159039354+Qonixxx@users.noreply.github.com>
2024-11-28 21:43:41 +0700

    Create 12.py

---
+++ егэ/tests /from Yandex (3-rd) /12.py
@@ -0,0 +1,8 @@
+s = "4" * 50
+
+while "444" in s or "333" in s:
+  if "444" in s: s = s.replace("444", "3", 1)
+  else: s = s.replace("333", "3344", 1)
+  if "3443" in s: s = s.replace("3443", "0", 1)
+
+print(s)
