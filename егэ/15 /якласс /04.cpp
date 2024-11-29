8e083541b5a204c13e7aff0ab55f76d03b010ef2
Qonixxx <159039354+Qonixxx@users.noreply.github.com>
2024-11-29 10:06:39 +0700

    Create 04.cpp

---
+++ егэ/15 /якласс /04.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    for (int a = 1; a <= 1000; a++) {
+        bool flag = true;
+        for (int x = 1; x <= 1000; x++) {
+            if ( ( (((x % 3) == 0) <= ((x % 4) != 0)) or ( (x + a) >= 138) ) == 0) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+			    cout << a << endl;
+			    break; 
+    	}	
+	  }
+}
