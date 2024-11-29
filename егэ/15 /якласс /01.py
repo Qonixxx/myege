b063cc0221943deef3f50532dae13bdee21f239e
Qonixxx <159039354+Qonixxx@users.noreply.github.com>
2024-11-29 09:14:08 +0700

    Create 01.py

---
+++ егэ/15 /якласс /01.py
@@ -0,0 +1,16 @@
+from itertools import combinations
+
+def f(x):
+    P = 25 <= x <= 50
+    Q = 30 <= x <= 90
+    A = a1 <= x <= a2
+    return ((not A) <= (not P)) <= (A <= Q)
+    
+ox = [i / 4 for i in range(25 * 4, 91 * 4)]
+
+    
+m = []
+for a1, a2 in combinations(ox, 2):
+    if all(f(x) == 1 for x in ox):
+        m.append(a2 - a1)
+print(max(m)) # 60
