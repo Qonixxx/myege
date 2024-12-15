0bb3c11035847f6a5168b605be05a521b6a6afce
Qonixxx <159039354+Qonixxx@users.noreply.github.com>
2024-12-15 14:21:47 +0700

    неравенства с максимальным значением

--- егэ/15 /all /08.py
+++ егэ/15 /all /08.py
@@ -3,4 +3,5 @@ def f(x, y, a): return ((2 * x + y) != 70) or (x < y) or (a < x)
 for a in range(1000, 0, -1):
     if all(f(x, y, a) == 1 for x in range(1, 500) for y in range(1, 500)):
         print(a)
-        break
+        break # ans - 28
+        
