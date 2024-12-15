25862faf998e42aeff7cd586bfa8f5b76376f441
Qonixxx <159039354+Qonixxx@users.noreply.github.com>
2024-12-15 15:34:36 +0700

    множества (минимальное && сумма)

---
+++ егэ/15 /all /10.py
@@ -0,0 +1,12 @@
+a = set()
+
+def f(x):
+    A = x in a
+    B = x in {1, 3, 5, 7, 9, 11}
+    C = x in {3, 6, 9, 12}
+    return (B <= (not C)) or A
+
+for x in range(1000):
+    if f(x) == 0:
+        a.add(x)
+print(a)
