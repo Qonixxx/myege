7475d06479a9201f7ef495873c75e7ca85023379
Qonixxx <159039354+Qonixxx@users.noreply.github.com>
2025-01-19 14:18:35 +0700

    киллер фичв для всех 27-х

--- егэ/27 /общее решение 27-ой /solve.py
+++ егэ/27 /общее решение 27-ой /solve.py
@@ -1,4 +1,4 @@
-with open("27.txt") as f:
+with open("27B.txt") as f:
     f.readline() # читаем первую строку "в пустоту"
     data = [] # список точек
     for s in f:
@@ -9,19 +9,19 @@ with open("27.txt") as f:
     # ф-ла в условии     
     def dist(p1, p2):
         x1, y1, x2, y2 = *p1, *p2
-        return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+        return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
     # автоматическое разделение на кластеры
     def getCluster(p0):
-        cluster = [p for p in data if dist(p0, p) < 1] # список из точек, которые находятся в ближайшей окрестности от p0
-        #  P.S если рассматривать файл B, то возможно расстояние м-у кластерами < 1, следовательно диапазон нужно уменьшить до 0.2
+        cluster = [p for p in data if dist(p0, p) < 0.3] # список из точек, которые находятся в ближайшей окрестности от p0
+        #  P.S если рассматривать файл B, то возможно расстояние м-у кластерами < 1, следовательно диапазон нужно уменьшить
         if len(cluster) > 0:
             for p in cluster: data.remove(p) # удаляем точки из общ.списка, чтобы повторно их не брать
             next_cluster = [getCluster(p) for p in cluster] # для тех точек, которых мы нашли, ищем соседей 
             cluster += sum(next_cluster, [])
         return cluster
     
-    clasters = [] 
+    clusters = [] 
     while len(data) > 0: # пока точки в списке есть
         p0 = data.pop() 
         cluster = [p0] + getCluster(p0) # берем первую точку и находим для нее класиер
