50d6c00c81fbbd787a746d20a66952086ef9c02e
Qonixxx <159039354+Qonixxx@users.noreply.github.com>
2025-02-12 10:25:48 +0700

    Update 27.py

--- егэ/tests /from Yandex (3-rd) /27.py
+++ егэ/tests /from Yandex (3-rd) /27.py
@@ -1,51 +1,48 @@
-# Доступны файлы для чтения: 27_A.txt, 27_B.txt, 27_A.xlsx, 27_B.xlsx
-# https://education.yandex.ru/ege/variants/5fc3c948-182b-495f-9d5a-acff30e93c71/task/27
 from math import *
+data = []
+coord = []
+def dist(p1, p2):
+    x1, y1, x2, y2 = *p1, *p2
+    return abs(x2 - x1) + abs(y2 - y1)
 
-with open("27_A.txt") as f:
-    data = []
-    for s in f:
-        p = [float(c) for c in s.split()]
-        data.append(p)
-    for i in range(len(data)):
-        data[i][0] = data[i][1] * cos(data[i][0])
-        data[i][1] = data[i][1] * sin(data[i][0])
-    # т.к по задаче первое знчение - угол, второе - радиус
-    # правила перевода в декартовую СК:
-    # x = r * cos(ф)
-    # y = r * sin(ф)
-
-#    print(data)
-
-    def dist(p1, p2):
-        x1, y1, x2, y2 = *p1, *p2
-        return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+def getCluster(p0):
+    kl = [p for p in data if dist(p0, p) < 0.3]
+    if len(kl) > 0:
+        for p in kl: data.remove(p)
+        nextkl = [getCluster(p) for p in kl]
+        for c in nextkl: kl.extend(c)
+    return kl
     
-    def getCluster(p0):
-        cluster = [p for p in data if dist(p0, p) < 1] # список из точек, которые находятся в ближайшей окрестности от p0
-        if len(cluster) > 0:
-            for p in cluster: data.remove(p) # удаляем точки из общ.списка, чтобы повторно их не брать
-            next_cluster = [getCluster(p) for p in cluster] # для тех точек, которых мы нашли, ищем соседей 
-            cluster += sum(next_cluster, [])
-        return cluster
+def center(kl):
+    m = []
+    for p in kl:
+        s = sum(dist(p, p1) for p1 in kl)
+        m.append([s, p])
+    return min(m)[1]
+    
+for s in open("27_B.txt"):
+    p = [float(c) for c in s.split()]
+    coord.append(p)
 
-    clusters = [] 
-    while len(data) > 0: # пока точки в списке есть
-        p0 = data.pop() 
-        cluster = [p0] + getCluster(p0) # берем первую точку и находим для нее класиер
-#       print(len(cluster))
-        clusters.append(cluster)
+for i in range(len(coord)):
+  a, r = coord[i]
+  x = r * cos(a)
+  y = r * sin(a)
+  data.append([x, y])
+# print(len(data))
 
-    def centr(kl):
-        m = []
-        for p in kl:
-            s = sum(dist(p, p1) for p1 in kl) # посчитали расстояние точки до каждой из точек кластора и сложили
-            m.append([s, p])
-        return min(m)[1]
+clusters = []
+while len(data) > 0:
+    p0 = data.pop()
+    cluster = [p0] + getCluster(p0)
+#    print(len(cluster))
+    clusters.append(cluster)
+# print(len(clusters))
+ 
+centroid = [center(kl) for kl in clusters]
 
-    centroid = [centr(kl) for kl in clusters]    
-    k = len(clusters)
+px = sum(x for x, y in centroid) / len(clusters)
+py = sum(y for x, y in centroid) / len(clusters)
+print(int(px * 10000), int(py * 10000))
 
-    px = sum(x for x, y in centroid)
-    py = sum(y for x, y in centroid)
-    print(int(px / k * 10000), int(py / k * 10000))
+    
