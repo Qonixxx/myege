47aa5eb407e042d6f8d0bac501656784d1537f35
Qonixxx <159039354+Qonixxx@users.noreply.github.com>
2025-03-03 19:59:55 +0700

    Update 05.py

--- егэ/15 /all /05.py
+++ егэ/15 /all /05.py
@@ -1,4 +1,4 @@
-def f(x, a): return (a % 7 == 0) and (240 % x ==0) <= ((a % x != 0) <= (780 % x != 0))
+def f(x, a): return (a % 7 == 0) and (240 % x == 0) <= ((a % x != 0) <= (780 % x != 0))
 
 for a in range(1, 1000):
     if all(f(x, a) == 1 for x in range(1, 10000)):
