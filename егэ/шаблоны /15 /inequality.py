502232262392ab76184e18508f6ed82e64a9355e
Qonixxx <159039354+Qonixxx@users.noreply.github.com>
2025-03-04 17:26:00 +0700

    шаблон на неравенства

---
+++ егэ/шаблоны /15 /inequality.py
@@ -0,0 +1,7 @@
+# 15-е (неравенства)
+def f(x, y, a): return ((x ** 2 - 10 * x + 16) > 0) or ((y ** 2 - 10 * y + 21) > 0) or ((x * y) < (2 * a))
+
+for a in range(1, 300):
+    if all(f(x, y, a) == 1 for x in range(1, 300) for y in range(1, 300)):
+        print(a)
+        break
