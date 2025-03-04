92190ca9dee234a56be2a53a266ee590f880b58e
Qonixxx <159039354+Qonixxx@users.noreply.github.com>
2025-03-04 16:49:50 +0700

    для графа

---
+++ егэ/шаблоны /01.py
@@ -0,0 +1,14 @@
+from itertools import *
+
+s1 = '1247 2148 3578 4126 538 647 7136 8235'
+s2 = 'ABHE BAH CEGD DFC EAGC FNGD GEFC HABF'
+s2 = {x[0]:set([1:]) for x in s2.split()}
+
+for p in permutations('ABCDEFH'):
+  s3 = s1
+  for x, y in zip('12345678', p):
+    s3 = s3.replace(x, y)
+  s3 = {x[0]:set(x[1:]) for x in s3.split()}
+  if s2 == s3:
+    print('1 2 3 4 5 6 7 8')
+    print(*p)
