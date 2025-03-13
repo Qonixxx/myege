1c874fced7018b8be06ebafcfa66ceee9939c59f
Qonixxx <159039354+Qonixxx@users.noreply.github.com>
2025-03-13 23:02:31 +0700

    делители, сумма макс и мин кроме единицы и числа

--- егэ/tests /from Gorbachev (5-th) /25.py
+++ егэ/tests /from Gorbachev (5-th) /25.py
@@ -2,8 +2,10 @@ def div(x): return sorted({i for d in range(2, int(x ** 0.5) + 1) if x % d == 0 
 k = 0
 for x in range(900000):
     if k == 7: break
-    m = str(sum(div(x)))
+    if len(div(x)) > 1:
+        m = max(div(x)) + min(div(x))
+    else: m = 0
+    m = str(m)
     if m[0] == "7" and m[-1] == "7":
         print(x, int(m))
         k += 1
-    
