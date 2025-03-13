9122d5aaf25145fb2d16e7435813cc478ffdeef3
Qonixxx <159039354+Qonixxx@users.noreply.github.com>
2025-03-13 22:50:23 +0700

    модуль, тройки

--- егэ/tests /from Gorbachev (5-th) /17.py
+++ егэ/tests /from Gorbachev (5-th) /17.py
@@ -1,6 +1,6 @@
 ans = []
 xs = [int(x) for x in open('17.txt')]
-def ch(x): return 10000 <= x < 10 ** 5 and x % 100 == 61
+def ch(x): return 10000 <= abs(x) < 10 ** 5 and abs(x) % 100 == 61
 mx33 = max(x for x in xs if 100 <= x <= 1000 and x % 100 == 33)
 
 for x, y, z in zip(xs, xs[1:], xs[2:]):
