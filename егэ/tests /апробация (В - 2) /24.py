6c0ec0116a0ca4e68aa02eaa53f091274b06b00c
Qonixxx <159039354+Qonixxx@users.noreply.github.com>
2025-04-08 22:23:36 +0700

    24

--- егэ/tests /апробация (В - 2) /24.py
+++ егэ/tests /апробация (В - 2) /24.py
@@ -2,7 +2,7 @@ s = open("24.txt").readline()
 l = mx = countAB = 0
 
 for r in range(1, len(s)):
-    if s[r] + s[r - 1] == "AB": countAB += 1
+    if s[r] + s[r - 1] == "BA": countAB += 1
     while countAB > 100:
         if s[l] + s[l + 1] == "AB": countAB -= 1
         l += 1
