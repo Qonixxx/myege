680430c64ea3626c5f691ff7e967dc75d1f2d386
Qonixxx <159039354+Qonixxx@users.noreply.github.com>
2025-04-08 19:25:07 +0700

    24

---
+++ егэ/tests /апробация (В - 2) /24.py
@@ -0,0 +1,10 @@
+s = open("24.txt").readline()
+l = mx = countAB = 0
+
+for r in range(1, len(s)):
+    if s[r] + s[r - 1] == "AB": countAB += 1
+    while countAB > 100:
+        if s[l] + s[l + 1] == "AB": countAB -= 1
+        l += 1
+    if countAB == 100: mx = max(mx, r - l + 1)
+print(mx)
