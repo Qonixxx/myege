4f6dcd36a3aed22876ed0aaf4f99f33e70b30d98
Qonixxx <159039354+Qonixxx@users.noreply.github.com>
2025-04-11 13:24:05 +0700

    Delete егэ/tests /досрок (2025) /2601.py

--- егэ/tests /досрок (2025) /2601.py
+++
@@ -1,9 +0,0 @@
-with open("26.txt") as f:
-    n = int(f.readline())
-    xs = sorted([int(x) for x in f], reverse = True)
-
-ans = []
-ans.append(xs[0])
-for i in range(1, len(xs)):
-    if ans[-1] - xs[i] >= 9: ans.append(xs[i])
-print(len(ans), ans[-1])
