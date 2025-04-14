e76cd6ece513ffa7c7643a30ef85973b4df151d9
Qonixxx <159039354+Qonixxx@users.noreply.github.com>
2025-04-14 21:56:36 +0700

    ZY или ZX

---
+++ егэ/24 /двойной цикл /07.py
@@ -0,0 +1,11 @@
+s = open("24.txt").readline()
+m = 0
+
+for l in range(len(s)):
+    for r in range(l + m, len(s)):
+        c = [l:r + 1]
+        if len(c) % 2 == 0:
+            if all(c[i] + c[i + 1] in ["ZX", "ZY"] for i in range(0, len(c), 2)):
+                m = max(m, len(c))
+            else: break
+print(m)
